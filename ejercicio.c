a176533021cc670d2114f5ef610da5cb30e6eb6c
Jose Ortega <jaod072211@gmail.com>
2026-05-12 11:31:25 -0500

    Primera opcion de impresion

---
+++ ejercicio.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int arreglo[]= {2 ,4 ,6 ,8 ,10};
+int *ptr;
+
+ptr=&arreglo[0];
+   for (int i=0; i< 5; i++)
+   {
+     printf("arreglo[%d] = %d ", i, arreglo[i]); 
+       printf(" %d\n ", *ptr);
+   }
+
+    return 0;
+}
