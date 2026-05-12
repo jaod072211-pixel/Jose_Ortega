ab6bdb82f87c6d0916f0775ea696e3b117511c0c
Jose Ortega <jaod072211@gmail.com>
2026-05-12 11:42:37 -0500

    Ejercicio 2

---
+++ parte1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    char vocales[]= {'a', 'e', 'i', 'o', 'u'};
+
+    for (int i = 0; i < 5; i++) {
+        printf("vocales[%d] = %c\n", i, vocales[i]);
+    }
+    return 0;
+}
